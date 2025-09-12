d94c2049a04f5b943948b06dd2f4244ac3a96fa2
j00sebox <josharms00@gmail.com>
2025-09-12 14:30:15 -0400

    Hello silksong :)

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using UnityEngine;
+
+[BepInPlugin("com.example.helloworld", "HelloWorld Mod", "1.0.0")]
+public class HelloWorld : BaseUnityPlugin
+{
+    void Start()
+    {
+        Logger.LogInfo("Hello, Silksong from VS Code!");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F5))
+        {
+            Logger.LogInfo("F5 was pressed!");
+        }
+    }
+}
